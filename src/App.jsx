857df464e2a668bd890dc744a0d77d54c005c49b
Ayudh <ayudhnandi@gmail.com>
2024-12-16 14:49:20 +0530

    fixed issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import Authentication from "./Components/Authentication";
+import Feed from "./Components/homecomponents/feed";
+import NewPost from "./Components/homecomponents/NewPost";
+import Profile from "./Components/homecomponents/Profile";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 function App() {
-  return <Authentication />;
+  return (
+    <Routes>
+      <Route path="/" element={<Authentication />} />
+      <Route path="/feed" element={<Feed />} />
+      <Route path="/newpost" element={<NewPost />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
 export default App;
