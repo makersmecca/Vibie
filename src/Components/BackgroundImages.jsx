0947fa70862ba13ab0e5c5d40c4c21fdbf76cffa
Ayudh <ayudhnandi@gmail.com>
2024-12-16 12:39:23 +0530

    adding image grid to bg

--- src/Components/BackgroundImages.jsx
+++ src/Components/BackgroundImages.jsx
@@ -1,5 +1,18 @@
+import bg1 from "/bgImg/bg1.jpeg";
+import bg2 from "/bgImg/bg2.jpeg";
+import bg3 from "/bgImg/bg3.jpeg";
+import bg4 from "/bgImg/bg4.jpeg";
+import bg5 from "/bgImg/bg5.jpeg";
+import bg6 from "/bgImg/bg6.jpeg";
+import bg7 from "/bgImg/bg7.jpeg";
+import bg8 from "/bgImg/bg8.jpeg";
 const BackgroundImages = () => {
-  return <></>;
+  return (
+    <>
+      <img src={bg1} className="h-[250px] rounded-lg m-2" />
+      <img src={bg1} className="h-[250px] rounded-lg m-2" />
+    </>
+  );
 };
 
 export default BackgroundImages;
