f0dbfa9eb2b98fc94e3b722e75ceb10c6fb2d2b9
Ayudh <ayudhnandi@gmail.com>
2024-12-16 17:15:21 +0530

    added new font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Hurricane: ["Hurricane", "sans-serif"],
+        Pacifico: ["Pacifico", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
