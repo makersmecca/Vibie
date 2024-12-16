376949765ece924fd250f865fa5d215d0ababcdb
Ayudh <ayudhnandi@gmail.com>
2024-12-16 13:58:16 +0530

    updated layout

--- src/Components/BackgroundImages.jsx
+++ src/Components/BackgroundImages.jsx
@@ -10,57 +10,79 @@ import bg9 from "/bgImg/bg9.jpeg";
 import bg10 from "/bgImg/bg10.jpeg";
 const BackgroundImages = () => {
   return (
-    <div className="flex flex-row gap-2 md:justify-center h-full">
-      {/* widescreen images */}
-      <div className="hidden md:block relative -top-10">
-        <img src={bg2} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg3} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg4} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg1} className="h-[250px] w-[200px] rounded-lg mt-2" />
-      </div>
-      <div className="hidden md:block relative -top-24">
-        <img src={bg9} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg1} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg8} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg1} className="h-[250px] w-[200px] rounded-lg mt-2" />
-      </div>
-      {/* smallscreen images */}
-      <div className="relative -top-24 -left-1 md:left-0">
-        <img src={bg1} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg2} className="h-[250px] w-[200px] rounded-lg mt-2" />
-      </div>
-      <div className="relative -top-5">
-        <img
-          src={bg8}
-          className="h-[250px] w-[300px] md:w-[200px] rounded-lg mt-2"
-        />
-        <img
-          src={bg4}
-          className="h-[250px] w-[300px] md:w-[200px] rounded-lg mt-2"
-        />
-      </div>
-      <div className="relative -top-32 -right-1 md:right-0">
-        <img
-          src={bg5}
-          className="h-[250px] w-[200px] md:w-w-[200px] rounded-lg mt-2"
-        />
-        <img
-          src={bg6}
-          className="h-[250px] w-[200px] md:w-w-[200px] rounded-lg mt-2"
-        />
-      </div>
-      {/* widescreen images */}
-      <div className="hidden md:block relative -top-10">
-        <img src={bg6} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg4} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg9} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg10} className="h-[250px] w-[200px] rounded-lg mt-2" />
-      </div>
-      <div className="hidden md:block relative -top-40">
-        <img src={bg7} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg10} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg5} className="h-[250px] w-[200px] rounded-lg mt-2" />
-        <img src={bg2} className="h-[250px] w-[200px] rounded-lg mt-2" />
+    <div className="h-screen overflow-hidden">
+      <div className="flex flex-row gap-2 md:justify-center h-full">
+        {/* widescreen images */}
+        <div className="hidden md:block relative -top-10">
+          <img src={bg2} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg3} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg4} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg1} className="h-[250px] w-[200px] rounded-lg mt-2" />
+        </div>
+        <div className="hidden md:block relative -top-24">
+          <img src={bg9} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg7} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg8} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg1} className="h-[250px] w-[200px] rounded-lg mt-2" />
+        </div>
+        {/* smallscreen images */}
+        <div className="relative -top-11 -left-1 md:left-0">
+          <img src={bg1} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg2} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img
+            src={bg7}
+            className="hidden md:block h-[250px] w-[200px] rounded-lg mt-2"
+          />
+          <img
+            src={bg10}
+            className="hidden md:block h-[250px] w-[200px] rounded-lg mt-2"
+          />
+        </div>
+        <div className="relative -top-48">
+          <img
+            src={bg8}
+            className="h-[250px] w-[300px] md:w-[200px] rounded-lg mt-2"
+          />
+          <img
+            src={bg4}
+            className="h-[250px] w-[300px] md:w-[200px] rounded-lg mt-2"
+          />
+          <img
+            src={bg10}
+            className="block md:hidden h-[250px] w-[200px] rounded-lg mt-2"
+          />
+        </div>
+        <div className="relative -top-32 -right-1 md:right-0">
+          <img
+            src={bg5}
+            className="h-[250px] w-[200px] md:w-w-[200px] rounded-lg mt-2"
+          />
+          <img
+            src={bg6}
+            className="h-[250px] w-[200px] md:w-w-[200px] rounded-lg mt-2"
+          />
+          <img
+            src={bg7}
+            className="hidden md:block h-[250px] w-[200px] rounded-lg mt-2"
+          />
+          <img
+            src={bg10}
+            className="hidden md:block h-[250px] w-[200px] rounded-lg mt-2"
+          />
+        </div>
+        {/* widescreen images */}
+        <div className="hidden md:block relative -top-10">
+          <img src={bg6} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg4} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg9} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg10} className="h-[250px] w-[200px] rounded-lg mt-2" />
+        </div>
+        <div className="hidden md:block relative -top-40">
+          <img src={bg7} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg10} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg5} className="h-[250px] w-[200px] rounded-lg mt-2" />
+          <img src={bg2} className="h-[250px] w-[200px] rounded-lg mt-2" />
+        </div>
       </div>
     </div>
   );
