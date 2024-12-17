fb8f31984b9223995440e91b9452e8acd77cc2b7
Ayudh <ayudhnandi@gmail.com>
2024-12-17 13:33:48 +0530

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Authentication from "./Components/Authentication";
 import Feed from "./Components/homecomponents/feed";
 import NewPost from "./Components/homecomponents/NewPost";
 import Profile from "./Components/userProfileComponents/Profile";
+import EditProfile from "./Components/userProfileComponents/EditProfile";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/feed" element={<Feed />} />
       <Route path="/newpost" element={<NewPost />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/editprofile" element={<EditProfile />} />
     </Routes>
   );
 }
