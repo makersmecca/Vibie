7a65d2c3c55ff02f4c442688d8677822dc211e22
Ayudh <ayudhnandi@gmail.com>
2024-12-18 12:23:21 +0530

    added provider context for current user to allow access to context in children

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import NewPost from "./Components/postsComponents/NewPost";
 import Profile from "./Components/userProfileComponents/Profile";
 import EditProfile from "./Components/userProfileComponents/EditProfile";
 import { Route, Routes } from "react-router-dom";
+import { UserProvider } from "./Components/UserContext";
+
 import "./App.css";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Authentication />} />
-      <Route path="/signup" element={<Authentication />} />
-      <Route path="/feed" element={<Feed />} />
-      <Route path="/newpost" element={<NewPost />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/editprofile" element={<EditProfile />} />
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<Authentication />} />
+        <Route path="/signup" element={<Authentication />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/newpost" element={<NewPost />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+      </Routes>
+    </UserProvider>
   );
 }
 
