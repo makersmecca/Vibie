dd75ec85075db3413a720b630b7d6187c859c56f
Ayudh <ayudhnandi@gmail.com>
2024-12-18 13:47:10 +0530

    added route to forgot password component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Feed from "./Components/homecomponents/feed";
 import NewPost from "./Components/postsComponents/NewPost";
 import Profile from "./Components/userProfileComponents/Profile";
 import EditProfile from "./Components/userProfileComponents/EditProfile";
+import ForgotPassword from "./Components/FogotPassword";
 import { Route, Routes } from "react-router-dom";
 import { UserProvider } from "./Components/UserContext";
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/newpost" element={<NewPost />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
       </Routes>
     </UserProvider>
   );
