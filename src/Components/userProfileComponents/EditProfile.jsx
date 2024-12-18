04fe7468de83ab24b318c96dbdec417f8f6e3f60
Ayudh <ayudhnandi@gmail.com>
2024-12-18 14:50:02 +0530

    updated back button positioning to show on widescreen

--- src/Components/userProfileComponents/EditProfile.jsx
+++ src/Components/userProfileComponents/EditProfile.jsx
@@ -33,28 +33,28 @@ const EditProfile = () => {
   };
   return (
     <div className="flex flex-col">
-      <Link to="/feed">
-        <div className="absolute left-5 top-6 z-10">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="white"
-            className="bi bi-arrow-left"
-            stroke="white"
-            strokeWidth="1.5"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
-            />
-          </svg>
-        </div>
-      </Link>
       {/* profile banner */}
       <div className="flex justify-center">
         <div className="relative w-full md:w-[800px]">
+          <Link to="/profile">
+            <div className="absolute left-5 top-6 z-10">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="white"
+                className="bi bi-arrow-left"
+                stroke="white"
+                strokeWidth="1"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+                />
+              </svg>
+            </div>
+          </Link>
           <img
             src={bg9}
             className="w-full md:w-[800px] h-[150px] rounded-b-2xl"
