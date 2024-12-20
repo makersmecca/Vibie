3651bdc1952fefbff4b735e9d5ca8f951d76b39d
Ayudh <ayudhnandi@gmail.com>
2024-12-20 17:28:36 +0530

    moved share functionality to a separate component

--- src/Components/postsComponents/Posts.jsx
+++ src/Components/postsComponents/Posts.jsx
@@ -14,6 +14,7 @@ import {
   doc,
   writeBatch,
 } from "firebase/firestore";
+import ShareButton from "../ShareButton";
 
 const Posts = () => {
   const { currentUser } = useContext(UserContext);
@@ -235,19 +236,7 @@ const Posts = () => {
             </div>
 
             {/* Share button */}
-            <button className="bg-slate-300 rounded-full w-[100px] py-1.5 px-1 flex items-center justify-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-send-fill mt-0.5"
-                viewBox="0 0 16 16"
-              >
-                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
-              </svg>
-              <span className="font-Lexend">Share</span>
-            </button>
+            <ShareButton postId={post.id} />
           </div>
 
           <div className="text-sm font-Lexend text-start text-gray-500 px-5 mt-2 ms-0.5">
