f1195fc039a214bb9cb78ff685a78063be0ffd10
makersmecca <ayudhnandi@gmail.com>
2024-12-21 00:10:48 +0530

    updated title

--- src/Components/Authentication.jsx
+++ src/Components/Authentication.jsx
@@ -249,7 +249,7 @@ const Authentication = () => {
       <div className="absolute inset-x-0 top-[26%] md:top-[5%] h-[100%] flex justify-center">
         <div className="bg-white border-black md:w-[40%] w-[90%] rounded-t-[50px] shadow-lg py-8 px-4">
           <span className="flex flex-col items-center w-full font-Pacifico text-[50px] mt-2 md:mt-12">
-            Vibesnap
+            InstaVibe
           </span>
           <span className="flex flex-col items-center w-full mt-3 font-Hurricane text-[32px]">
             Join the #vibetribe
