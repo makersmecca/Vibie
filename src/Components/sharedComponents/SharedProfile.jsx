230d98580fa5a504b3de7b3363827b7c162e8b09
makersmecca <ayudhnandi@gmail.com>
2024-12-22 02:00:46 +0530

    added loading animations

--- src/Components/sharedComponents/SharedProfile.jsx
+++ src/Components/sharedComponents/SharedProfile.jsx
@@ -96,9 +96,25 @@ const SharedProfile = () => {
         <div className="relative w-full md:w-[800px]">
           <div
             className={`bg-gray-400 rounded-b-2xl ${
-              bannerImgUrl === "" ? "w-full md:w-[800px] h-[150px]" : ""
+              bannerImgUrl === ""
+                ? `w-full md:w-[800px] h-[150px] ${
+                    isLoading &&
+                    "animate-pulse flex justify-center items-center"
+                  }`
+                : ""
             }`}
           >
+            {isLoading && (
+              <svg
+                className="w-10 h-10 text-gray-200 animate-pulse"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 18"
+              >
+                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+              </svg>
+            )}
             {bannerImgUrl !== "" && (
               <img
                 src={bannerImgUrl}
@@ -111,92 +127,141 @@ const SharedProfile = () => {
       {/* profile picture */}
       <div className="absolute top-[13%] md:top-[15%] left-[5%] sm:left-[10%] lg:left-[30%]">
         <div className="w-[100px] h-[100px] bg-white rounded-full flex items-center justify-center">
-          <img
-            src={profileImgUrl}
-            className="md:relative w-[94px] h-[94px] rounded-full bg-white"
-          />
+          {isLoading ? (
+            <svg
+              className="md:relative w-[94px] h-[94px] text-gray-400 animate-pulse"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+            </svg>
+          ) : (
+            <img
+              src={profileImgUrl}
+              className="md:relative w-[94px] h-[94px] rounded-full bg-white"
+            />
+          )}
         </div>
       </div>
 
       {/* username */}
       <div className="self-center w-full md:w-[800px] mt-[90px] text-3xl font-semibold font-Lexend px-7">
-        {username}
+        {isLoading ? (
+          <div className="animate-pulse">
+            <div className="h-3 my-3 bg-gray-200 rounded-full w-32 mb-2"></div>
+          </div>
+        ) : (
+          <>{username}</>
+        )}
       </div>
       {/* bio */}
-      <div className="self-center w-full md:w-[800px] px-7 mt-3">{bio}</div>
+      <div className="self-center w-full md:w-[800px] px-7 mt-3">
+        {isLoading ? (
+          <div className="animate-pulse">
+            <div className="h-3 bg-gray-200 rounded-full w-[300px] mb-2"></div>
+          </div>
+        ) : (
+          <>{bio}</>
+        )}
+      </div>
       {/* user's posts */}
       <div className="self-center w-full md:w-[800px] mx-2 px-7 text-xl md:text-2xl font-medium sticky top-0 bg-white py-4">
         Posts
       </div>
-      {/* div showing the user's posts */}
-      <div className="overflow-y-auto flex justify-center">
-        <div className="flex flex-col gap-6">
-          {posts.map((post) => (
-            <div
-              key={post.id}
-              className="w-[350px] rounded-2xl shadow bg-gray-200 flex flex-col py-5 mb-4"
+      {isLoading ? (
+        <>
+          <div className="flex justify-center items-center h-40 gap-5">
+            {/* <div className="animate-spin rounded-full h-8 w-8 border-t-[2px] border-gray-900"></div> */}
+            <svg
+              aria-hidden="true"
+              className="w-8 h-8 text-gray-200 animate-spin fill-gray-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <div className="flex justify-between items-center">
-                <div className="w-full h-16 items-center flex px-5 gap-7">
-                  <div className="w-[52px] h-[52px] bg-white rounded-full flex items-center justify-center">
-                    <img
-                      src={profileImgUrl}
-                      className="w-12 h-12 rounded-full"
-                    />
-                  </div>
-                  <div className="">
-                    <div className="font-Lexend font-medium text-xl">
-                      {username}
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <div className="animate-pulse font-Lexend">Loading Posts...</div>
+          </div>
+        </>
+      ) : (
+        <div className="overflow-y-auto flex justify-center">
+          <div className="flex flex-col gap-6">
+            {posts.map((post) => (
+              <div
+                key={post.id}
+                className="w-[350px] rounded-2xl shadow bg-gray-200 flex flex-col py-5 mb-4"
+              >
+                <div className="flex justify-between items-center">
+                  <div className="w-full h-16 items-center flex px-5 gap-7">
+                    <div className="w-[52px] h-[52px] bg-white rounded-full flex items-center justify-center">
+                      <img
+                        src={profileImgUrl}
+                        className="w-12 h-12 rounded-full"
+                      />
+                    </div>
+                    <div className="">
+                      <div className="font-Lexend font-medium text-xl">
+                        {username}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <img
-                className="rounded-xl w-[90%] max-h-[300px] self-center"
-                src={post.mediaUrl}
-                alt=""
-              />
+                <img
+                  className="rounded-xl w-[90%] max-h-[300px] self-center"
+                  src={post.mediaUrl}
+                  alt=""
+                />
 
-              <div className="flex justify-between px-5 mt-5 items-center">
-                <div className="flex items-center justify-between w-[60px]">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="#ff708f"
-                    className="bi bi-heart-fill ms-1"
-                    viewBox="0 0 16 16"
-                    onClick={() => handleLikes(post.id)}
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-                    />
-                  </svg>
+                <div className="flex justify-between px-5 mt-5 items-center">
+                  <div className="flex items-center justify-between w-[60px]">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="#ff708f"
+                      className="bi bi-heart-fill ms-1"
+                      viewBox="0 0 16 16"
+                      onClick={() => handleLikes(post.id)}
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                      />
+                    </svg>
 
-                  <span className="text-lg text-gray-600 font-medium font-Lexend">
-                    {post.likeCount || 0}
-                  </span>
+                    <span className="text-lg text-gray-600 font-medium font-Lexend">
+                      {post.likeCount || 0}
+                    </span>
+                  </div>
+                  <ShareButton postId={post.id} />
                 </div>
-                <ShareButton postId={post.id} />
-              </div>
-              <div className="text-sm font-Lexend text-start text-gray-500 px-5 mt-2 ms-0.5">
-                {post.createdAt?.seconds
-                  ? new Date(post.createdAt.seconds * 1000).toLocaleDateString(
-                      "en-US",
-                      {
+                <div className="text-sm font-Lexend text-start text-gray-500 px-5 mt-2 ms-0.5">
+                  {post.createdAt?.seconds
+                    ? new Date(
+                        post.createdAt.seconds * 1000
+                      ).toLocaleDateString("en-US", {
                         day: "numeric",
                         month: "short",
                         year: "numeric",
-                      }
-                    )
-                  : ""}
+                      })
+                    : ""}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
