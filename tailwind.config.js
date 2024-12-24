4b54f21d3abaef03b6297eaf26ff10311291ad1c
Ayudh <ayudhnandi@gmail.com>
2024-12-24 13:43:59 +0530

    added custom animation class to animte like button click

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ export default {
         Pacifico: ["Pacifico", "sans-serif"],
         Lexend: ["Lexend", "sans-serif"],
       },
+      animation: {
+        likeButton: "like 0.5s ease-in-out",
+      },
+
+      keyframes: {
+        like: {
+          "0%": { transform: "scale(1)" },
+          "25%": { transform: "scale(1.2)" },
+          "50%": { transform: "sacle(0.95)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
     },
   },
   plugins: [],
