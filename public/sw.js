7af6759da5bddc0cda01762fb78c2628ad863de8
Ayudh Nandi <37550248+makersmecca@users.noreply.github.com>
2024-12-26 08:22:09 +0000

    updating service worker file

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,19 @@
-const cacheVer = "1.0.0";
+const OFFLINE_VER = "1.0.1";
+const CACHE_NAME = "vbe"+OFFLINE_VER;
+const OFFLINE_URL = "fallback.html";
+
+const assets = [
+    "/",
+    "/index.html",
+    "/static/js/main.bundle.js", // Your main React bundle
+    "/manifest.json",
+    "fallback.html",
+    "Icons/icon-512x512.png",
+    "Icons/icon-384x384.png",
+    "Icons/icon-192x192.png",
+    "Icons/icon-152x152.png",
+    "Icons/icon-144x144.png",
+    "Icons/icon-128x128.png",
+    "Icons/icon-96x96.png",
+    "Icons/icon-72x72.png",
+];
