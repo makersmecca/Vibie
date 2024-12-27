b75c461746fbf9ae5eddd48b175b1a0a0c497e81
Ayudh Nandi <37550248+makersmecca@users.noreply.github.com>
2024-12-27 07:26:36 +0000

    updated layout breakpoint dimensions

--- src/Components/Authentication.jsx
+++ src/Components/Authentication.jsx
@@ -378,9 +378,9 @@ const Authentication = () => {
               <Link to="/">Already have an account? Login.</Link>
             )}
           </span>
-          <div className="md:flex md:justify-evenly mt-4 md:mt-8 px-2">
+          <div className="xl:flex xl:justify-evenly mt-4 md:mt-8 px-2">
             <div className="justify-self-center">©2024 Copyright Vibie</div>
-            <div className="flex justify-evenly self-center mt-5 md:mt-0 md:gap-2">
+            <div className="flex justify-evenly self-center mt-4 md:mt-0 md:gap-2">
               <div className="underline">
                 <Link to="/termsofuse">Terms of Use</Link>
               </div>
