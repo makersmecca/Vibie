abdb74d52e5dab32915f34ebdb22ab064e16a5e8
Ayudh Nandi <37550248+makersmecca@users.noreply.github.com>
2024-12-27 07:40:19 +0000

    minor update

--- src/Components/Authentication.jsx
+++ src/Components/Authentication.jsx
@@ -380,7 +380,7 @@ const Authentication = () => {
           </span>
           <div className="flex flex-col xl:flex-row items-center xl:justify-center mt-4 md:mt-8 px-2 gap-2 xl:gap-6">
             <div className="">©2024 Copyright Vibie</div>
-            <div className="flex justify-evenly self-center md:gap-2">
+            <div className="flex justify-evenly self-center gap-1 md:gap-2">
               <div className="underline">
                 <Link to="/termsofuse">Terms of Use</Link>
               </div>
