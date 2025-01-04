2d586c3f61c6607f728a18f59ffea59ebfcbad82
makersmecca <ayudhnandi@gmail.com>
2025-01-04 15:37:51 +0530

    added like functionality to shared post and added condition to check if user is currently signed in

--- src/Components/sharedComponents/SharedPost.jsx
+++ src/Components/sharedComponents/SharedPost.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../auth/firebaseAuth";
 import { Link } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
 import { Client, Storage } from "appwrite";
+import LikeButton from "../LikeButton";
+import ShareButton from "./ShareButton";
 
 const PostDetails = () => {
+  const { currentUser } = useContext(UserContext);
   const { postID } = useParams();
   const [post, setPost] = useState(null);
   const [postUser, setPostUser] = useState(null);
@@ -72,7 +76,6 @@ const PostDetails = () => {
           setUserID(docSnap.data().userId);
           const userProfile = await fetchUserProfile(docSnap.data().userId);
           setPostUser(userProfile);
-
           const fileId = url.includes("/preview")
             ? url.split("/files/")[1].split("/preview")[0]
             : url.split("/files/")[1].split("/view")[0];
@@ -110,39 +113,11 @@ const PostDetails = () => {
     };
 
     fetchPost();
-  }, [postID]);
+  }, [postID, currentUser]);
 
-  // if (loading) {
-  //   return (
-  //     <div className="flex justify-center items-center h-screen gap-4">
-  //       {/* <div className="animate-spin rounded-full h-8 w-8 border-t-[2px] border-gray-900"></div> */}
-  //       <svg
-  //         aria-hidden="true"
-  //         className="w-8 h-8 text-gray-200 animate-spin fill-gray-600"
-  //         viewBox="0 0 100 101"
-  //         fill="none"
-  //         xmlns="http://www.w3.org/2000/svg"
-  //       >
-  //         <path
-  //           d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-  //           fill="currentColor"
-  //         />
-  //         <path
-  //           d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-  //           fill="currentFill"
-  //         />
-  //       </svg>
-  //       <div className="animate-pulse font-Lexend">Loading Post...</div>
-  //     </div>
-  //   );
-  // }
-
-  // if (!post)
-  //   return (
-  //     <div className="flex w-full justify-center h-screen items-center font-Lexend font-lg">
-  //       Uh oh! Post not found
-  //     </div>
-  //   );
+  useEffect(() => {
+    // console.log(post);
+  }, [post]);
 
   return (
     <div className="flex w-full justify-center h-screen items-center">
@@ -202,38 +177,26 @@ const PostDetails = () => {
           </div>
           <>{post.mediaUrl && content}</>
           <div className="flex justify-between px-5 mt-5 items-center">
-            <div className="flex items-center justify-between w-[60px]">
-              <button disabled>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="#ff708f"
-                  className="bi bi-heart-fill ms-1"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-                  />
-                </svg>
-              </button>
-              <span className="text-lg text-gray-600 font-medium font-Lexend">
-                {post.likeCount || 0}
-              </span>
-            </div>
-            <div className="text-sm font-Lexend text-start text-gray-500 px-5 ms-0.5">
-              {post.createdAt?.seconds
-                ? new Date(post.createdAt.seconds * 1000).toLocaleDateString(
-                    "en-US",
-                    {
-                      day: "numeric",
-                      month: "short",
-                      year: "numeric",
-                    }
-                  )
-                : ""}
-            </div>
+            <LikeButton
+              postId={postID}
+              userId={post.userId}
+              initialLikeCount={post.likeCount}
+              initialLikedBy={post.likedBy}
+              currentUser={currentUser}
+            />
+            <ShareButton postId={postID} />
+          </div>
+          <div className="text-sm font-Lexend text-start text-gray-500 px-5 ms-0.5">
+            {post.createdAt?.seconds
+              ? new Date(post.createdAt.seconds * 1000).toLocaleDateString(
+                  "en-US",
+                  {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  }
+                )
+              : ""}
           </div>
         </div>
       ) : (
